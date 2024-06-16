9662245375711e9bc25bf10c967c4f1ca642168b
Akeem Rotimi <akeemrotimi10@gmail.com>
2024-06-16 22:38:14 +0100

    Removed unused import

--- app/src/main/java/com/akeemrotimi/vpdmoneyassessment/ui/feature/home/HomeFragment.kt
+++ app/src/main/java/com/akeemrotimi/vpdmoneyassessment/ui/feature/home/HomeFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableIntStateOf
@@ -13,7 +12,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.room.Room
 import com.akeemrotimi.vpdmoneyassessment.R
@@ -23,7 +21,6 @@ import com.akeemrotimi.vpdmoneyassessment.data.model.Transaction
 import com.akeemrotimi.vpdmoneyassessment.ui.feature.account.AccountViewModel
 import com.akeemrotimi.vpdmoneyassessment.ui.feature.account.AccountViewModelFactory
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
 
